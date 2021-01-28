0dab7fda3119153bbb91333042dfc091822b28fe
Paola Pagotto <55468847+paolapagotto@users.noreply.github.com>
2021-01-28 19:11:18 -0300

    Revert "facebook login"

--- DrugData/Scenes/Login/View/LoginViewController.swift
+++ DrugData/Scenes/Login/View/LoginViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import Firebase
 import GoogleSignIn
-import FBSDKLoginKit
 
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
@@ -17,9 +16,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textFieldEmail: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var signInButton: GIDSignInButton!
-    @IBOutlet weak var signInButtonFB: FBLoginButton!
-    
-    let signInButtonFB = FBLoginButton()
     
     @IBAction func buttonForgotPassword(_ sender: Any) {
         if let forgotPassword = UIStoryboard(name: "EsqueceuASenhaViewController", bundle: nil).instantiateInitialViewController() as? EsqueceuASenhaViewController {
@@ -42,6 +38,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                         alertController.addAction(UIAlertAction(title: "Aceitar", style: .default))
                         
                         self.present(alertController, animated: true, completion: nil)
+                    
                     
                     }
                     cleanTextFields()
@@ -106,24 +103,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance().signIn()
         
-        
-        signInButtonFB.center = view.center
-        view.addSubview(signInButtonFB)
-        if let token = AccessToken.current,
-                !token.isExpired {
-                // User is logged in, do work such as go to next view controller.
-            }
-        signInButtonFB.permissions = ["public_profile", "email"]
-   
     }
+   
 }
+
 extension UIViewController {
     class func replaceRootViewController(viewController: UIViewController) {
         guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first
